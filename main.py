dc6f07c893e6e39ec28f2a4f3f75c611079ccaad
星星旁の旷野 <xingkuangye@icloud.com>
2026-03-13 00:43:15 +0800

    Refactor MyPlugin to implement markdown testing commands and remove unused methods

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.1")
+@register("md超级测试", "星星旁の旷野", "毁灭世界", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("文字")
     async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, **你了 {message_str}!**") # 发送一条纯文本消息
-
-    async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        """测试md文字"""
+        yield event.plain_result("当你看到这段话，证明纯文字md测试正常") 
+        yield event.plain_result("""**当你看到这段被加粗，证明粗体文字md测试正常**
+*当你看到这段被斜体，证明斜体文字md测试正常*
+~~当你看到这段被删除线，证明删除线md测试正常~~
+__当你看到这段被下划线，证明下划线md测试正常__
+***当你看到这段被加粗斜体，证明加粗斜体md测试正常***
+当你看到这段被代码块包裹，证明代码块md测试正常
+```
+print('Hello World')
+```""") 
